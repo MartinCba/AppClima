29f6543ebc2cd71235137214f7e8c6891f66a592
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2024-12-26 12:36:44 -0300

    mostrar data

--- script.js
+++ script.js
@@ -24,18 +24,28 @@ function mostrarDatosClima(data) {
 
   const ciudadNombre = data.name;
   const temperatura = data.main.temp;
+  const humedad = data.main.humidity;
+  const viento = data.wind.speed;
   const descripcion = data.weather[0].description;
 
   const ciudadTitulo = document.createElement("h2");
   ciudadTitulo.textContent = ciudadNombre;
 
   const temperaturaInfo = document.createElement("p");
-  temperaturaInfo.textContent = `Temperatura: ${temperatura - kelvinDiff}°C`;
+  temperaturaInfo.textContent = `Temperatura: ${Math.round(temperatura - kelvinDiff)}°C`;
+
+  const humedadInfo = document.createElement("p");
+  humedadInfo.textContent = `Humedad: ${humedad}%`;
+
+  const vientoInfo = document.createElement("p");
+  vientoInfo.textContent = `Viento: ${viento * 3.6.toFixed(2)} k/h`;
 
   const descripcionInfo = document.createElement("p");
   descripcionInfo.textContent = `Descripción: ${descripcion}`;
 
   datosClima.appendChild(ciudadTitulo);
   datosClima.appendChild(temperaturaInfo);
+  datosClima.appendChild(humedadInfo);
+  datosClima.appendChild(vientoInfo);
   datosClima.appendChild(descripcionInfo);
 }

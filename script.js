830490a705eca054a451e5a9686c7351235c609e
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2024-12-26 14:23:27 -0300

    icono agregado

--- script.js
+++ script.js
@@ -23,13 +23,15 @@ function mostrarDatosClima(data) {
   datosClima.innerHTML = "";
 
   const ciudadNombre = data.name;
+  const paisNombre = data.sys.country;
   const temperatura = data.main.temp;
   const humedad = data.main.humidity;
   const viento = data.wind.speed;
   const descripcion = data.weather[0].description;
+  const icono = data.weather[0].icon;
 
   const ciudadTitulo = document.createElement("h2");
-  ciudadTitulo.textContent = ciudadNombre;
+  ciudadTitulo.textContent = `${ciudadNombre}, ${paisNombre}`;
 
   const temperaturaInfo = document.createElement("p");
   temperaturaInfo.textContent = `Temperatura: ${Math.round(temperatura - kelvinDiff)}°C`;
@@ -38,12 +40,16 @@ function mostrarDatosClima(data) {
   humedadInfo.textContent = `Humedad: ${humedad}%`;
 
   const vientoInfo = document.createElement("p");
-  vientoInfo.textContent = `Viento: ${viento * 3.6.toFixed(2)} k/h`;
+  vientoInfo.textContent = `Viento: ${(viento * 3.6).toFixed(2)} k/h`;
+
+  const iconoImagen = document.createElement("img");
+  iconoImagen.src = `https://openweathermap.org/img/wn/${icono}.png`;
 
   const descripcionInfo = document.createElement("p");
   descripcionInfo.textContent = `Descripción: ${descripcion}`;
 
   datosClima.appendChild(ciudadTitulo);
+  datosClima.appendChild(iconoImagen);
   datosClima.appendChild(temperaturaInfo);
   datosClima.appendChild(humedadInfo);
   datosClima.appendChild(vientoInfo);
